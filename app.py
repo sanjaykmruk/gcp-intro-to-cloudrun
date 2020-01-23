2522f9bdff40ecd1335dec44bc76c741a7d63ebd
sanjaykmruk <sanjay.kumar@armakuni.com>
2020-01-23 14:47:59 +0000

    simulating build failure

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello_world():
 @app.route('/<name>')
 def hello_recipient(name):
 
-    if random.randint(1,6) == 6:    # commented to simulated build failure
+   # if random.randint(1,6) == 6:    # commented to simulated build failure
         raise Exception("Internal error accourred")
 
     return jsonify({"message": f"Welcome {name}"})
