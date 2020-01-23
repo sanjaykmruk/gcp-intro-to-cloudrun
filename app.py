25df25c2fd2f6cc05ff5c867fa23fe5e82a4e9b2
sanjaykmruk <sanjay.kumar@armakuni.com>
2020-01-23 14:44:52 +0000

    hello -> welcome

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def hello_recipient(name):
     if random.randint(1,6) == 6:    # commented to simulated build failure
         raise Exception("Internal error accourred")
 
-    return jsonify({"message": f"Hello {name}"})
+    return jsonify({"message": f"Welcome {name}"})
 
 if __name__ == "__main__":
     app.run()
