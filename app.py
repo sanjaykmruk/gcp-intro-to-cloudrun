f9769565932a36ad9379e45f53f9b22479db56b9
sanjaykmruk <sanjay.kumar@armakuni.com>
2020-01-23 14:29:13 +0000

    simulating build failure

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import random
+import sys
 
 from flask import Flask, jsonify
 
@@ -9,6 +11,14 @@ def hello_world():
     recipient = os.getenv("RECIPIENT", "world")
 
     return jsonify({"message": "Hello", "recipient": recipient})
+
+@app.route('/<name>')
+def hello_recipient(name):
+
+    # if random.randint(1,6) == 6:    # commented to simulated build failure
+        raise Exception("Internal error accourred")
+
+    return jsonify({"message": f"Hello {name}"})
 
 if __name__ == "__main__":
     app.run()
